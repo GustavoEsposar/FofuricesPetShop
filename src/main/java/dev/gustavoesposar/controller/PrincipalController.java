8ce1c9104ee0f09aabc1f34826fe9a77d954baea
guesposar <guesposar@icloud.com>
2024-05-15 21:51:16 -0300

    adição de identificadores no principal.fxml

---
+++ src/main/java/dev/gustavoesposar/controller/PrincipalController.java
@@ -0,0 +1,32 @@
+package dev.gustavoesposar.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
+public class PrincipalController {
+
+    @FXML
+    private Tab tabCategorias;
+
+    @FXML
+    private Tab tabEspecies;
+
+    @FXML
+    private Tab tabEstoque;
+
+    @FXML
+    private Tab tabFornecedores;
+
+    @FXML
+    private Tab tabMarcas;
+
+    @FXML
+    private Tab tabPets;
+
+    @FXML
+    private Tab tabProdutos;
+
+    @FXML
+    private Tab tabRacas;
+
+}
