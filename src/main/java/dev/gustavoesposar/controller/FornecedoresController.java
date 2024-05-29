8fc359a710e1e099463c011a5ac7e08878d1c94c
guesposar note <guesposar@icloud.com>
2024-05-29 08:25:23 -0300

    metodo de inicialização e remocao adicionados

--- src/main/java/dev/gustavoesposar/controller/FornecedoresController.java
+++ src/main/java/dev/gustavoesposar/controller/FornecedoresController.java
@@ -1,13 +1,18 @@
 package dev.gustavoesposar.controller;
 
+import dev.gustavoesposar.controller.abstracts.OpcaoDoMenu;
+import dev.gustavoesposar.database.DatabaseManager;
+import dev.gustavoesposar.model.Fornecedor;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class FornecedoresController {
+public class FornecedoresController extends OpcaoDoMenu{
+    private final String sqlDelete = "DELETE FROM Fornecedor WHERE idFornecedor = ? LIMIT 1;";
 
     @FXML
     private Button btnAdd;
@@ -19,25 +24,25 @@ public class FornecedoresController {
     private Button btnUp;
 
     @FXML
-    private TableColumn<?, ?> colCnpj;
+    private TableColumn<Fornecedor, String> colCnpj;
 
     @FXML
-    private TableColumn<?, ?> colEmail;
+    private TableColumn<Fornecedor, String> colEmail;
 
     @FXML
-    private TableColumn<?, ?> colIdPet;
+    private TableColumn<Fornecedor, Integer> colIdFornecedor;
 
     @FXML
-    private TableColumn<?, ?> colNomeFantasia;
+    private TableColumn<Fornecedor, String> colNomeFantasia;
 
     @FXML
-    private TableColumn<?, ?> colRazaoSocial;
+    private TableColumn<Fornecedor, String> colRazaoSocial;
 
     @FXML
-    private TableColumn<?, ?> colTelefone;
+    private TableColumn<Fornecedor, String> colTelefone;
 
     @FXML
-    private TableView<?> tbl;
+    private TableView<Fornecedor> tbl;
 
     @FXML
     private TextField txtCnpj;
@@ -69,11 +74,51 @@ public class FornecedoresController {
 
     @FXML
     private void remover(ActionEvent event) {
+        String idSelecionado = txtId.getText();
 
+        boolean sucesso = DatabaseManager.executarUpdate(sqlDelete, idSelecionado);
+
+        super.processarResultado(sucesso);
+        restaurarValoresVariaveis();
     }
 
     @FXML
     private void voltarMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void initialize() {
+        configurarColunasTableView();
+        ajustarLarguraColunas();
+        atualizarTabela();
+    }
+
+    private void configurarColunasTableView() {
+        colIdFornecedor.setCellValueFactory(new PropertyValueFactory<>("idFornecedor"));
+        colNomeFantasia.setCellValueFactory(new PropertyValueFactory<>("nomeFantasia"));
+        colRazaoSocial.setCellValueFactory(new PropertyValueFactory<>("razaoSocial"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
+        colCnpj.setCellValueFactory(new PropertyValueFactory<>("cnpj"));
+    }
+
+    private void ajustarLarguraColunas() {
+        colIdFornecedor.prefWidthProperty().bind(tbl.widthProperty().multiply(0.1));
+        colNomeFantasia.prefWidthProperty().bind(tbl.widthProperty().multiply(0.18));
+        colRazaoSocial.prefWidthProperty().bind(tbl.widthProperty().multiply(0.18));
+        colEmail.prefWidthProperty().bind(tbl.widthProperty().multiply(0.18));
+        colTelefone.prefWidthProperty().bind(tbl.widthProperty().multiply(0.18));
+        colCnpj.prefWidthProperty().bind(tbl.widthProperty().multiply(0.18));
+    }
+
+    @Override
+    protected void atualizarTabela() {
+;
+    }
+
+    @Override
+    protected void restaurarValoresVariaveis() {
 
     }
 
