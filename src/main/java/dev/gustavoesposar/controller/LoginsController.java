7e4aad1fdc0d14f06d943323c914f8e182e07072
guesposar <guesposar@icloud.com>
2024-05-29 15:31:10 -0300

    LoginsController: adição e refresh da tabela funcionando

--- src/main/java/dev/gustavoesposar/controller/LoginsController.java
+++ src/main/java/dev/gustavoesposar/controller/LoginsController.java
@@ -1,8 +1,14 @@
 package dev.gustavoesposar.controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import dev.gustavoesposar.controller.abstracts.OpcaoDoMenu;
 import dev.gustavoesposar.database.DatabaseManager;
 import dev.gustavoesposar.model.Login;
+import dev.gustavoesposar.utils.PasswordUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,6 +20,13 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class LoginsController extends OpcaoDoMenu{
     private final String sqlDelete = "DELETE FROM Login WHERE idLogin = ? LIMIT 1;";
+    private final String sqlSelect = "SELECT idLogin, email FROM login;";
+    private final String sqlInsert =    "INSERT INTO login (email, senha)\r\n" + //
+                                        "SELECT ?, ? FROM DUAL\r\n" + //
+                                        "WHERE NOT EXISTS (\r\n" + //
+                                        "    SELECT 1 FROM login WHERE email = ?\r\n" + //
+                                        ")\r\n" + //
+                                        "";
 
     @FXML
     private Button btnAdd;
@@ -44,12 +57,29 @@ public class LoginsController extends OpcaoDoMenu{
 
     @FXML
     private void adicionar(ActionEvent event) {
+        String email = txtEmail.getText();
+        String senha = PasswordUtil.hashPassword(txtSenha.getText());
 
+        boolean sucesso = false;
+        if (btnAdd.getText().equals("Update")) {
+            btnAdd.setText("Adicionar");
+            //sucesso = DatabaseManager.executarUpdate(sqlUpdatePet, valor, raca, fornecedor, txtId.getText());
+        } else {
+            sucesso = DatabaseManager.executarUpdate(sqlInsert, email, senha, email);
+        }
+
+        processarResultado(sucesso);
+        restaurarValoresVariaveis();
     }
 
     @FXML
     private void atualizar(ActionEvent event) {
+        if (btnAdd.getText().equals("Update")) {
+            return;
+        }
 
+        btnAdd.setText("Update");
+        
     }
 
     @FXML
@@ -64,7 +94,21 @@ public class LoginsController extends OpcaoDoMenu{
 
     @Override
     protected void atualizarTabela() {
+        ObservableList<Login> loginsList = FXCollections.observableArrayList();
 
+        try(ResultSet res = DatabaseManager.executarConsulta(sqlSelect)) {
+            while (res.next()) {
+                int id = res.getInt("idLogin");
+                String email = res.getString("email");
+
+                Login login = new Login(id, email);
+                loginsList.add(login);
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        tbl.setItems(loginsList);
     }
 
     @Override
