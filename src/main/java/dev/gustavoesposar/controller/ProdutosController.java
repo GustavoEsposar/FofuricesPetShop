a72da22c073c1ae1bb5af1c4d6dc5e9f30e243c3
guesposar <guesposar@icloud.com>
2024-06-03 14:47:07 -0300

    ajustes nas Views

--- src/main/java/dev/gustavoesposar/controller/ProdutosController.java
+++ src/main/java/dev/gustavoesposar/controller/ProdutosController.java
@@ -16,7 +16,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TableView;  
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
